05af3d5844c183ce22c734cc180175a99ae2563b
cwymbewe <cwymbewe@gmail.com>
2025-02-10 23:11:23 +0200

    `Refactor App component to use Login component instead of the default react App component

---
+++ frontend/react/src/components/Login.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AlphaLogo from '../images/AlphaLogo.png'
+
+const Login = () => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const response = await fetch('/api/login', { 
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }), 
+
+        });
+
+        if (response.ok) {
+            navigate('/stock-form'); 
+
+        } else {
+            alert('Login failed. Please check your credentials.');
+        }
+    };
+
+    return (
+        <div>
+            <h1>Alpha LP Gas Stock Management</h1>
+            <img src={AlphaLogo} alt="Alpha Logo" />
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
